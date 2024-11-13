e4e1b81268b443aa40ecfd8dceeae14cb043caee
Alex Gunnarsson <alexgu@kth.se>
2024-11-14 00:10:09 +0100

    done

--- src/gaussfft.py
+++ src/gaussfft.py
@@ -4,4 +4,26 @@ from numpy.fft import fft2, ifft2, fftshift
 def gaussfft(pic, t):
     # Fill in your code here
     # result = ...  
-    return result
+
+    # 1. Filter based on sampled Gaussian
+    [sizex, sizey] = pic.shape
+    X, Y = np.meshgrid(
+        range(-int(sizex/2), int(sizex/2)), 
+        range(-int(sizey/2), int(sizey/2))
+        # np.linspace(-sizex/2, sizex/2, sizex), 
+        # np.linspace(-sizey/2, sizey/2, sizey)
+    )
+    gauss = 1/(2*np.pi*t) * np.exp(-1/(2*t) * (X**2+Y**2))
+
+    # 2. Fourier transform image and Gaussian filter
+    pic_fft = fft2(pic)
+    gauss_fft = fft2(fftshift(gauss))
+
+    # 3. Multiply Fourier transforms
+    result_fft = pic_fft * gauss_fft
+
+    # 4. Invert the resulting Fourier transform
+    result = ifft2(result_fft)
+    # result = fftshift(result)
+
+    return np.real(result)
--- src/lab1.py
+++ src/lab1.py
@@ -14,7 +14,15 @@ from fftwave import fftwave
 # depending on what you are working on. It should be fairly easy for a TA
 # to go through all parts of your code though.
 
-EXERCISE = 1.9
+# EXERCISE = 1.3
+# EXERCISE = 1.4
+# EXERCISE = 1.5
+# EXERCISE = 1.6
+# EXERCISE = 1.7
+# EXERCISE = 1.8
+# EXERCISE = 2.3
+# EXERCISE = 3.1
+EXERCISE = 3.2
 
 # Fhat = np.zeros((128, 128))
 # for i in range(32):
@@ -76,7 +84,7 @@ match EXERCISE:
 			fig.add_subplot(5, 3, i*3+2).set_title("alpha = " + str(alpha))
 			showfs(Ghat, display=False)
 
-			Hhat = rot(fftshift(Ghat), -alpha)
+			Hhat = fftshift(rot(Ghat), -alpha)
 			fig.add_subplot(5, 3, i*3+3).set_title("alpha = " + str(alpha))
 			showgrey(np.log(1 + abs(Hhat)), display=False)
 		plt.show()
@@ -101,11 +109,88 @@ match EXERCISE:
 			showgrey(data, display=False)
 		plt.show()
 		
-	case 1.9:
-		print("blur")
+	case 2.3:
+		T = [0.1, 0.3, 1.0, 10.0, 100.0]
+		for t in T:
+			pic = deltafcn(128, 128)
+			gauss_fft = gaussfft(pic, t)
+			discgauss_fft = discgaussfft(pic, t)
+			print(f"t = {t}:")
+			print(variance(gauss_fft))
+			print(f"disc t = {t}:")
+			print(variance(discgauss_fft))
+
+		T = [1.0, 4.0, 16.0, 64.0, 256.0]
+		images = [
+			np.load("Images-npy/phonecalc128.npy"),
+			np.load("Images-npy/few128.npy"),
+			np.load("Images-npy/nallo128.npy")
+		]
+		fig = plt.figure(1)
+		for i, img in enumerate(images):
+			fig.add_subplot(3, 6, i*6+1).set_title("Raw")
+			showgrey(img, display=False)
+			for j, t in enumerate(T):
+				fig.add_subplot(3, 6, i*6+2+j).set_title("t = " + str(t))
+				showgrey(gaussfft(img, t), display=False)
+		plt.show()
 		
+	case 3.1:
+		gauss_param = [0.5, 1.0, 1.5, 2.0]
+		med_param = [2, 4, 6, 8]
+		lowpass_param = [0.1, 0.125, 0.15, 0.175]
+		collection = [gauss_param, med_param, lowpass_param]
 
+		def plotComparison(img, title):
+			fig = plt.figure(1)
 
+			fig.add_subplot(4, 4, 1).set_title("Raw")
+			office = np.load("Images-npy/office256.npy")
+			showgrey(office, display=False)
+
+			fig.add_subplot(4, 4, 2).set_title(title)
+			showgrey(img, display=False)
+
+			for i, params in enumerate(collection):
+				for j, t in enumerate(params):
+					match i:
+						case 0:
+							s = f"Gauss t={t}"
+							img2 = discgaussfft(img, t)
+						case 1:
+							s = f"Median w={t}"
+							img2 = medfilt(img, t)
+						case 2:
+							s = f"Lowpass cutoff={t}"
+							img2 = ideal(img, t)
+					fig.add_subplot(4, 4, 5 + i*4 + j).set_title(s)
+					showgrey(img2, display=False)
+			plt.show()
+
+		office = np.load("Images-npy/office256.npy")
+		add = gaussnoise(office, 16)
+		sap = sapnoise(office, 0.1, 255)
+		plotComparison(add, "Gauss Noise")
+		plotComparison(sap, "Sap Noise")
+		
+	case 3.2:
+		img = np.load("Images-npy/phonecalc256.npy")
+		smoothimg = img
+		N = 5
+		f = plt.figure()
+		f.subplots_adjust(wspace=0, hspace=0)
+		for i in range(N):
+			if i>0: # generate subsampled versions
+				img = rawsubsample(img)
+				# smoothimg = gaussfft(smoothimg, 0.5) # <call_your_filter_here>(smoothimg, <params>)
+				# smoothimg = discgaussfft(smoothimg, 0.5) # <call_your_filter_here>(smoothimg, <params>)
+				smoothimg = ideal(smoothimg, 0.25) # <call_your_filter_here>(smoothimg, <params>)
+				smoothimg = rawsubsample(smoothimg)
+			f.add_subplot(2, N, i + 1)
+			showgrey(img, False)
+			f.add_subplot(2, N, i + N + 1)
+			showgrey(smoothimg, False)
+		plt.show()
 
 # Exercise 1.3
 if 0:
